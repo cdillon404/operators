668fc6b803b8294dc6c9e71eae869dd16f196323
cdillon404 <chavin@gmail.com>
2020-11-16 21:16:58 -0700

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
+require 'pry'
+
 def unsafe?(speed)
-
+    if speed > 60 || speed < 40
+        return true
+    end
+    false
 end
-
-
 
 def not_safe?(speed)
-	
+    return speed > 60 || speed < 40 ? true : false
 end
-	
-
-
